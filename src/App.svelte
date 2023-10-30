9bc01e19b8b2bf1703ede8d0ddded2a4a4b11408
Silvestrs <SilvestrsL47@gmail.com>
2023-10-30 13:55:04 +0200

    Add Files

---
+++ src/App.svelte
@@ -0,0 +1,68 @@
+<style>
+  table, tr, td, th {
+    border: 1px solid black;
+    border-collapse: collapse;
+    border-style: groove;
+    padding: 0.5rem;
+    border-radius: 0.2rem;
+  }
+  code {
+    color: rgb(129, 213, 32);
+    padding: 0.2rem;
+    background-color: rgb(245, 245, 245);
+    border-radius: 0.2rem;
+  }
+</style>
+
+<main>
+    <table>
+      <tr>
+        <th>Termins Bri'ish</th>
+        <th>Termins Latviski</th>
+      </tr>
+      <tr>
+        <td><code>feature</code></td>
+        <td><code>Funkcionalitāte / Fīča</code></td>
+      </tr>
+      <tr>
+        <td><code>background</code></td>
+        <td><code>Konteksts / Situācija</code></td>
+      </tr>
+      <tr>
+        <td><code>scenario</code></td>
+        <td><code>Piemērs / Scenārijs</code></td>
+      </tr>
+      <tr>
+        <td><code>scenarioOutline</code></td>
+        <td><code>Scenārijs pēc parauga</code></td>
+      </tr>
+      <tr>
+        <td><code>examples</code></td>
+        <td><code>Piemēri / Paraugs<code></td>
+      </tr>
+      <tr>
+        <td><code>given</code></td>
+        <td><code>* / Kad</code></td>
+      </tr>
+      <tr>
+        <td><code>when</code></td>
+        <td><code>* / Ja</code></td>
+      </tr>
+      <tr>
+        <td><code>then</code></td>
+        <td><code>* / Tad</code></td>
+      </tr>
+      <tr>
+        <td><code>and</code></td>
+        <td><code>* / Un</code></td>
+      </tr>
+      <tr>
+        <td><code>but</code></td>
+        <td><code>* / Bet</code></td>
+      </tr>
+      <tr>
+        <td><code>rule</code></td>
+        <td><code>Rule</code></td>
+      </tr>
+    <table/>
+</main>
